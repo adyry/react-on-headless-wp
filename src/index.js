d69163684b0349bb154efecbc3ff88271e0cc5b4
Adrian <akowalczewski@light4website.com>
2017-10-22 23:32:58 +0200

    service workers+project flex fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import './scss/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+registerServiceWorker();
